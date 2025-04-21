e5a3d139a57ac77722953565863f16ddceeb85cd
bimmbim <bimabaraja76@gmail.com>
2025-04-21 12:56:26 +0700

    commit1

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// deklarasi array tanpa ukuran, wajib diisi datanya
+int nilai[] = {1, 2, 4, 7};
+
+// deklarasi array dengan ukuran dan diberi nilai awal
+
+
